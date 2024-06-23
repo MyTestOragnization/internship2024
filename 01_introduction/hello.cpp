149933132ac7433a4b154a5cc98b571ed7cf7a32
adamswiech <adam.swiech@vulcan.edu.pl>
2024-06-23 14:57:24 +0200

    hello program

---
+++ 01_introduction/hello.cpp
@@ -0,0 +1,17 @@
+// C++ program to display "Hello World"
+
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+
+// Main() function: where the execution of
+// program begins
+int main()
+{
+    // Prints hello
+    cout << "Welcome students to this year's internships!!!" << endl
+         << endl;
+
+    //    system("Pause");
+    return 0;
+}
