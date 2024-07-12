daa8daf8adfc10d4bfd9d1018888292fe6b32a8f
Daniel <g.danylko2007@gmail.com>
2024-07-12 09:38:06 +0200

    update in getnumbers

--- students/Daniel/cpp/primeNumbers.cpp
+++ students/Daniel/cpp/primeNumbers.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 // array of numbers 2...30
 auto getNumbers(){
-   vector<int> numbers {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+   vector<int> numbers;
+   for(int insertNumbers=2; insertNumbers<=30;insertNumbers++){
+    numbers.push_back(insertNumbers);
+   }
    return numbers;
 }
 
