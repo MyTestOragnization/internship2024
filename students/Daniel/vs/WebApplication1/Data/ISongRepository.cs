1213a26de209318c6c72823b011043a57e426b6f
Daniel <g.danylko2007@gmail.com>
2024-07-29 12:51:02 +0200

    new files

---
+++ students/Daniel/vs/WebApplication1/Data/ISongRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public interface ISongRepository
+    {
+        public List<Piosenka> GetAllSongs();
+        public bool EditSong(int Id, Piosenka song);
+        public Piosenka GetSongById(int SongID);
+        public bool DeleteSong(int SongID);
+        public bool AddNewSong(Piosenka piosenka);
+    }
+}
